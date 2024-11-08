c6b34d7a9571b535b56f99f28db2223151e29c3a
Satyam Sisodiya <studentofcode.satyam@gmail.com>
2024-11-08 21:01:17 +0530

    Add Create Listing Page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useSelector} from 'react-redux'
+import { Link } from 'react-router-dom';
 import {
   updateUserStart,
   updateUserSuccess,
@@ -113,6 +114,9 @@ export default function Profile() {
           className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
         >
           {loading ? 'Loading...' : 'Update'}</button>
+          <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
 
       </form>
       <div className="flex justify-between mt-5">
